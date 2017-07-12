10d6e3241ac6ec8252ebc39c26a64995be986d40
edwinmartinez <martinmedia@yahoo.com>
2017-07-12 15:38:08 -0700

    mysql setup and pollapp template

--- teslapoll/pollapp/views.py
+++ teslapoll/pollapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('<h2>this is pollapp</h2>')
+	return render(request, 'pollapp/index.html')
